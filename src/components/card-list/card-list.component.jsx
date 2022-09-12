ae3c111b8c5ad0744da4e4d6bf6eb9f342b817f5
NexTRahul <rahulpaul0009@gmail.com>
2022-09-13 02:35:43 +0530

    Initial Commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./card-list.style.css";
+import { Card } from "../card/card.component";
+
+export const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {props.monstars.map((monstar) => (
+        <Card key={monstar.id} monstar={monstar} />
+      ))}
+    </div>
+  );
+};
